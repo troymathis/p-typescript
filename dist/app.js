b63b3499fa5052d52b82ef971e0c7cc829c709d4
Troy Mathis <troy.mathis00@gmail.com>
2023-01-31 23:51:54 -0600

    just trying out some things

--- dist/app.js
+++ dist/app.js
@@ -6,8 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+const oneOfUs = 1;
+const bedroomWindow = "half-open";
+const user1 = {
+    name: "troy",
+    id: 1,
+};
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send(`${user1.name}`);
 });
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,32 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+
+type myBool = true | false;
+type WindowStates = "open" | "closed" | "minimized";
+type LockStates = "locked" | "unlocked";
+type PositiveOddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
+type StringArray = Array<string>;
+type NumberArray = Array<number>;
+type ObjWithNameArray = Array<{name:string}>;
+
+const oneOfUs: PositiveOddNumbersUnderTen = 1
+const bedroomWindow: WindowStates = "open";
+const s1: StringArray = ["yo","welcome"];
+const objname: ObjWithNameArray = [{name: "hey"}]
+
+interface User {
+    name: string;
+    id: number;
+}
+
+const user1: User = {
+    name: "troy",
+    id: 1,
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`${user1.name}`);
 });
 
 app.listen(port, () => {
